6a8c5168d903409e428b09ca124cfe1339dc01c4
Savchenko <savchenko@phys.vsu.ru>
2022-07-11 21:49:01 +0300

    get data from API by Axios

--- src/summary/Summary.js
+++ src/summary/Summary.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import api from "../api";
 
 function Summary() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        api.get("/summary")
+            .then(res => setData(res.data));
+    }, []);
+
     return (
-        <div>
-            Summary
-        </div>
+        <div>Summary</div>
     )
 }
 
